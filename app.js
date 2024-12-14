e2da7dbab3625157bcb1f73143fc1333c5a31c39
Car583-gh <carlafuensalida8@gmail.com>
2024-12-14 20:28:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const pool = require('./db'); //importa la conexion a la base de datos
 const routes = require('./routes');
-const app = express();
-const cors = require('cors');
 
+const app = express();
+const PORT = 3000;
+
+//Middleware
+app.use(bodyParser.json()); //Para procesar JSON
 app.use(cors()); // Habilita CORS para todas las rutas
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api', routes); //montar ruta en /apis
 app.use('/', routes);
 
-
-app.listen(3000, () => {
-    console.log('Servidor ejecutandose en el puerto 3000');
+app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`);
 });
