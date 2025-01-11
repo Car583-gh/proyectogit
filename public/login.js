5be060bd68f19469ef55b1a74125b3a94b555779
Car583-gh <carlafuensalida8@gmail.com>
2025-01-10 23:43:30 -0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -28,3 +28,38 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
 .catch(error => console.error('Error: ', error));
     });
 });
+document.addEventListener('DOMContentLoaded',() => {
+    //Manejo del formulario de login
+document.getElementById('loginForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const dni = document.getElementById('dni').value;
+    const rolId = document.getElementById('rol').value;
+
+    //Depuracion: verificar los datos capturados
+    console.log({ dni, rolId });
+
+    // Enviar los datos al servidor para verificar el login
+    fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ dni, rol_id: rolId })
+    })
+    .then(response => response.json())
+    .then(data => {
+        //Depuracion verificar la respuesta del servidor
+        console.log(data);
+        if(data.success) {
+            if(data.token) {
+        localStorage.setItem('token', data.token); // Guardar el token
+    }
+        alert('Login exitoso');
+        console.log('Usuario autenticado:', data.usuario);
+        window.location.href = 'http://localhost:3000/dashboard.html'; // Redirigir al dashboard despues del login
+    } else {
+        alert('Credenciales incorrectas: ' + data.message);
+    }
+})
+.catch(error => console.error('Error: ', error));
+    });
+});
