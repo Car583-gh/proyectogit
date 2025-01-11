b7576a323d193b9b29c4ed44d9cea9af28060dc8
Car583-gh <carlafuensalida8@gmail.com>
2025-01-10 23:34:12 -0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,4 +1,13 @@
-function logout() {
-    localStorage.removeItem('token');
-    window.location.href = '/login.html';
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('cerrarSesionBtn').addEventListener('click', cerrarSesion);
+});
+
+function cerrarSesion() {
+    // Limpiar almacenamiento de sesión
+    sessionStorage.clear();
+    localStorage.clear();
+
+    // Redirigir a la página de inicio de sesión
+    window.location.href = 'login.html'; // Verifica que esta ruta es correcta
 }
+
