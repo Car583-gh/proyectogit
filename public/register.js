a22bcea9c044a89742a947d0236add05d3ca673e
Car583-gh <carlafuensalida8@gmail.com>
2025-01-10 23:39:26 -0300

    Update register.js

--- public/register.js
+++ public/register.js
@@ -6,13 +6,14 @@ document.getElementById('registerForm').addEventListener('submit', (e) => {
     const nombre = document.getElementById('nombre').value;
     const dni = document.getElementById('dni').value;
     const correo = document.getElementById('correo').value;
-    const rol_id = document.getElementById('rol').value;
+    const rol_id = document.getElementById('rol_id').value;
+    const rol_nombre = document.getElementById('rol_nombre').value;
 
     // Enviar los datos al servidor para registrar el usuario
     fetch('http://localhost:3000/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ nombre, dni, correo, rol_id })
+        body: JSON.stringify({ nombre, dni, correo, rol_id, rol_nombre })
     })
     .then(response => response.json())
     .then(data => {
@@ -24,5 +25,5 @@ document.getElementById('registerForm').addEventListener('submit', (e) => {
         }
     })
     .catch(error => console.error('Error:', error));
-});
+    });
 });
